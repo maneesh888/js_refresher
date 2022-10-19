d402e652c5fa8052da9e4e33fd9be4be0548ecc7
Maneesh <maneesh888@gmail.com>
2022-10-19 20:00:33 +0400

    Object Arrays and Array methods

--- play.js
+++ play.js
@@ -20,14 +20,38 @@ age = 31;
 //     return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
 // }
 
-const summarizerUser = (userName, userAge, userHasHobby) =>  'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
+// const summarizerUser = (userName, userAge, userHasHobby) =>  'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
 
-const add = (a,b) => a + b;
-const addOne = a => a + 1
-const addRandom = () => 6+7;
+// const add = (a,b) => a + b;
+// const addOne = a => a + 1
+// const addRandom = () => 6+7;
 
-console.log(add(1,2))
-console.log(addOne(5));
-console.log(addRandom());
-console.log(summarizerUser(name, age, hasHobbies));
+// console.log(add(1,2))
+// console.log(addOne(5));
+// console.log(addRandom());
+// console.log(summarizerUser(name, age, hasHobbies));
+
+const person = {
+    name:'Maneesh',
+    age:31,
+    // greet: function () {
+    //     console.log('Hi, I am ' + this.name)
+    // }
+    greet() {
+        console.log('Hi, I am ' + this.name)
+    }
+}
+//person.greet();
+const hobbies = ['Sports', 'Cooking']
+
+for (let hobby of hobbies) {
+    console.log(hobby)
+}
+
+// console.log(hobbies.map(hobby => {
+//     return 'Hobby: ' + hobby
+// }));
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+
 
