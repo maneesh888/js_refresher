a1e621e4c0931c0e90c85899cf45e85ee820c196
Maneesh <maneesh888@gmail.com>
2022-10-19 20:31:54 +0400

    Async func and callbacks

---
+++ async.js
@@ -0,0 +1,16 @@
+const fetchData = callBack => {
+    setTimeout(()=> {
+        console.log('Done!');
+    }, 1500)
+}
+
+setTimeout(()=> {
+    console.log('Timer is done!');
+    fetchData(text => {
+        console.log(text);
+    })
+}, 2000)
+
+console.log('Hi');
+console.log('Hello');
+
