d86ad4f58f27d8bd058a41eeebf618cfe8fa98d4
Maneesh <maneesh888@gmail.com>
2022-10-19 19:29:17 +0400

    Different types of function calling

--- play.js
+++ play.js
@@ -4,11 +4,30 @@ const hasHobbies = true;
 
 age = 31;
 
-function summarizerUser(userName, userAge, userHasHobby) {
-    return (
-        'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
-    )
-}
+// const summarizerUser = function (userName, userAge, userHasHobby) {
+//     return (
+//         'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
+//     )
+// }
 
+// const summarizerUser = (userName, userAge, userHasHobby) => {
+//     return (
+//         'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
+//     )
+// }
+
+// const summarizerUser = (userName, userAge, userHasHobby) => {
+//     return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
+// }
+
+const summarizerUser = (userName, userAge, userHasHobby) =>  'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
+
+const add = (a,b) => a + b;
+const addOne = a => a + 1
+const addRandom = () => 6+7;
+
+console.log(add(1,2))
+console.log(addOne(5));
+console.log(addRandom());
 console.log(summarizerUser(name, age, hasHobbies));
 
